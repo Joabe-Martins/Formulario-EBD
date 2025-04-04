be277261148d8817fbca8e337d0207c874de1277
Joabe Martins <joabemartins1@gmail.com>
2025-04-03 21:10:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// URL do seu Google Apps Script para integração com a planilha
+// Substitua o link abaixo pelo link da sua integração (Google Apps Script publicado como Web App)
+const scriptURL = 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbybaj3gDaHUWfqWGd7gNvWtMDK1haERjmZbt31YFh67SmearzrpLgXX0Ztvhe4B3NgTZQ/exec'; // INSIRA AQUI O SEU LINK DE INTEGRAÇÃO
+
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault(); // Impede o comportamento padrão do formulário
+
+  // Envia os dados do formulário via fetch utilizando o método POST
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      // Após o envio bem-sucedido, redireciona para a página de agradecimento
+      window.location.href = 'thankyou.html';
+    })
+    .catch(error => console.error('Erro!', error.message));
+});

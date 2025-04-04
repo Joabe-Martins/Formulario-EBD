ca7d9f977ac7efba7ed21e71ae5304fc99c1c625
Joabe Martins <joabemartins1@gmail.com>
2025-04-03 21:54:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // URL do seu Google Apps Script para integração com a planilha
 // Substitua o link abaixo pelo link da sua integração (Google Apps Script publicado como Web App)
-const scriptURL = "https://script.google.com/macros/s/AKfycbxlyEvla8ucJ9FW4-3eiEkD95fIKYxZmxqLNSiMXvS7if93vXzw37ixE3ZTdvQEZBNl/exec"; // INSIRA AQUI O SEU LINK DE INTEGRAÇÃO
+const scriptURL = "https://script.google.com/macros/s/AKfycbzf43StZ37ZBn15D716GXD57wXQdIW-46_IjPNtXDZyUJAvpXiEJndMZ3r5rnOnuXXI/exec"; // INSIRA AQUI O SEU LINK DE INTEGRAÇÃO
 
 const form = document.getElementById('myForm');
 
